a68a59763f38b50e5ff60c2497a3f955870bdbd4
Martyna <mburghardt@student.agh.edu.pl>
2025-10-13 12:25:28 +0200

    Lotto

---
+++ Lotto.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Lotto {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Set<Integer> liczby = new HashSet<>();
+
+        while (liczby.size() < 6) {
+            int liczba = random.nextInt(49) + 1;
+            liczby.add(liczba);
+        }
+
+        System.out.println("Wylosowane:" + liczby);
+    }
+}
